49c6f6ab4e1710701be07f54f8a1261b2985eec2
kimdw524 <kimdw524@naver.com>
2025-07-15 23:32:26 +0900

    chore: install axios

---
+++ apps/web/src/api/axiosInstance.ts
@@ -0,0 +1,11 @@
+'use client';
+
+import axios from 'axios';
+
+export const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
