43eef309905ec3e0cbcb351559dcee510dcbf72a
kimdw524 <kimdw524@naver.com>
2025-07-16 01:06:03 +0900

    feat: Chip color 속성 추가

--- packages/ui/src/components/Chip/Chip.tsx
+++ packages/ui/src/components/Chip/Chip.tsx
@@ -7,9 +7,9 @@ import * as s from './Chip.css';
 
 type ChipProps = UIComponent<'div', typeof s.chip>;
 
-export const Chip = ({ children, className, size = 'md', sx: propSx, ...props }: ChipProps) => {
+export const Chip = ({ children, className, color = 'primary', size = 'md', sx: propSx, ...props }: ChipProps) => {
   return (
-    <div className={clsx(s.chip({ size }), className, sx(propSx))} {...props}>
+    <div className={clsx(s.chip({ color, size }), className, sx(propSx))} {...props}>
       <span>{children}</span>
     </div>
   );
