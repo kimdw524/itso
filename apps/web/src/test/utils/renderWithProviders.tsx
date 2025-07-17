21f89923ebb2c7c7738abb1186d2b5e1526cb864
kimdw524 <kimdw524@naver.com>
2025-07-18 04:41:40 +0900

    test: renderWithProviders 테스트 유틸 구현

---
+++ apps/web/src/test/utils/renderWithProviders.tsx
@@ -0,0 +1,9 @@
+import { render, RenderResult } from '@testing-library/react';
+
+import { UIProvider } from '@repo/ui';
+
+export const renderWithProviders = (
+  children: React.ReactElement,
+): RenderResult => {
+  return render(<UIProvider>{children}</UIProvider>);
+};
