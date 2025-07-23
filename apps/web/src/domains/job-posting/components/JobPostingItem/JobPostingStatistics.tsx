778832af0ac436121dd626bf1045889323591c41
kimdw524 <kimdw524@naver.com>
2025-07-23 16:37:39 +0900

    feat: 채용공고 마감기한 표시하기

--- apps/web/src/domains/job-posting/components/JobPostingItem/JobPostingStatistics.tsx
+++ apps/web/src/domains/job-posting/components/JobPostingItem/JobPostingStatistics.tsx
@@ -4,13 +4,24 @@ import { EyeIcon, StarIcon } from 'lucide-react';
 
 import { Box, Typography } from '@repo/ui';
 
+import { formatTime, getDday } from '@/utils/date';
+
 import type { JobPostingSummary } from '../../types/job-posting';
 
 const Item = ({ icon, value }: { icon: ReactNode; value: ReactNode }) => {
   return (
-    <Box flex gap="sm" alignItems="center" sx={{ color: 'secondary-foreground' }}>
+    <Box
+      flex
+      gap="sm"
+      alignItems="center"
+      sx={{ color: 'secondary-foreground' }}
+    >
       {icon}
-      <Typography color="secondary-foreground" fontSize="xs" fontWeight="medium">
+      <Typography
+        color="secondary-foreground"
+        fontSize="xs"
+        fontWeight="medium"
+      >
         {value}
       </Typography>
     </Box>
@@ -21,11 +32,20 @@ interface JobPostingStatisticsProps {
   jobPosting: JobPostingSummary;
 }
 
-export const JobPostingStatistics = ({ jobPosting }: JobPostingStatisticsProps) => {
+export const JobPostingStatistics = ({
+  jobPosting,
+}: JobPostingStatisticsProps) => {
   return (
-    <Box flex justifyContent="flex-end" gap="lg">
-      <Item icon={<EyeIcon />} value={<>{jobPosting.views}</>} />
-      <Item icon={<StarIcon />} value={<>{jobPosting.views}</>} />
+    <Box flex alignItems="center" justifyContent="space-between" gap="lg">
+      <Typography fontSize="sm">
+        {jobPosting.dueDate === null
+          ? '상시채용'
+          : `~ ${formatTime(jobPosting.dueDate)} (${getDday(jobPosting.dueDate)})`}
+      </Typography>
+      <Box flex gap="lg">
+        <Item icon={<EyeIcon />} value={<>{jobPosting.views}</>} />
+        <Item icon={<StarIcon />} value={<>{jobPosting.views}</>} />
+      </Box>
     </Box>
   );
 };
--- apps/web/src/domains/job-posting/components/JobPostingItem/index.tsx
+++ apps/web/src/domains/job-posting/components/JobPostingItem/index.tsx
@@ -1,4 +1,12 @@
-import { Box, Card, CardContent, CardInteraction, CardThumbnail, Chip, Typography } from '@repo/ui';
+import {
+  Box,
+  Card,
+  CardContent,
+  CardInteraction,
+  CardThumbnail,
+  Chip,
+  Typography,
+} from '@repo/ui';
 
 import type { CompanySummary } from '@/domains/company/types/company';
 
@@ -7,15 +15,19 @@ import { formatEmploymentType } from '../../utils';
 import { formatJobName } from '../../utils/formatJobName';
 import { ExperienceRangeChip } from './ExperienceRangeChip';
 import { JobPostingStatistics } from './JobPostingStatistics';
+import * as s from './style.css';
 
 interface JobPostingItemProps {
   jobPosting: JobPostingSummary;
   company: CompanySummary;
 }
 
-export const JobPostingItem = ({ jobPosting, company }: JobPostingItemProps) => {
+export const JobPostingItem = ({
+  jobPosting,
+  company,
+}: JobPostingItemProps) => {
   return (
-    <Card sx={{ backgroundColor: 'background' }}>
+    <Card className={s.container} sx={{ backgroundColor: 'background' }}>
       <CardInteraction>
         {/* 회사 로고 이미지 */}
         <Box
@@ -25,7 +37,11 @@ export const JobPostingItem = ({ jobPosting, company }: JobPostingItemProps) => 
           padding="xl"
           style={{ height: '7rem', backgroundColor: '#fff' }}
         >
-          <CardThumbnail src={company.logo || ''} alt="logo" style={{ maxWidth: '75%', maxHeight: '100%' }} />
+          <CardThumbnail
+            src={company.logo || ''}
+            alt="logo"
+            style={{ maxWidth: '75%', maxHeight: '100%' }}
+          />
         </Box>
         <CardContent sx={{ padding: 'xl' }}>
           {/* 회사 이름 */}
@@ -33,13 +49,23 @@ export const JobPostingItem = ({ jobPosting, company }: JobPostingItemProps) => 
             {jobPosting.company.name}
           </Typography>
           {/* 공고 제목 */}
-          <Typography fontSize="md" fontWeight="medium" lineHeight="sm" style={{ height: '2.5em', overflow: 'hidden' }}>
+          <Typography
+            fontSize="md"
+            fontWeight="medium"
+            lineHeight="sm"
+            style={{ height: '2.5em', overflow: 'hidden' }}
+          >
             {jobPosting.title}
           </Typography>
           {/* 태그 */}
           <Box flex flexWrap="wrap" gap="md" marginY="lg">
-            <ExperienceRangeChip min={jobPosting.minExperience} max={jobPosting.maxExperience} />
-            <Chip color="blue">{formatEmploymentType(jobPosting.employmentType)}</Chip>
+            <ExperienceRangeChip
+              min={jobPosting.minExperience}
+              max={jobPosting.maxExperience}
+            />
+            <Chip color="blue">
+              {formatEmploymentType(jobPosting.employmentType)}
+            </Chip>
             <Chip color="zinc">{formatJobName(jobPosting.jobId)}</Chip>
           </Box>
           <JobPostingStatistics jobPosting={jobPosting} />
