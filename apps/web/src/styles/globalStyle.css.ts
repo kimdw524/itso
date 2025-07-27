38c64302702ba5afd75a49d151e49bf8c698854f
kimdw524 <kimdw524@naver.com>
2025-07-27 23:16:45 +0900

    feat: 스크롤바 css 스타일 작성

--- apps/web/src/styles/globalStyle.css.ts
+++ apps/web/src/styles/globalStyle.css.ts
@@ -1,5 +1,9 @@
 import { fontFace, globalStyle } from '@vanilla-extract/css';
 
+import { theme } from '@repo/ui/themes';
+
+import { STYLE } from '@/constants/style';
+
 const pretendard = fontFace({
   src: 'url("/fonts/PretendardVariable.woff2")',
 });
@@ -11,5 +15,22 @@ globalStyle('*', {
 globalStyle('main', {
   margin: '0 auto',
   width: '100%',
-  maxWidth: '1440px',
+  maxWidth: STYLE.CONTAINER_WIDTH,
+});
+
+globalStyle('*::-webkit-scrollbar', {
+  width: '0.5rem',
+  height: '0.5rem',
+});
+
+globalStyle('*::-webkit-scrollbar-thumb', {
+  backgroundColor: `rgba(${theme.color.accent}, 0.66)`,
+  borderRadius: theme.borderRadius,
+});
+globalStyle('*::-webkit-scrollbar-thumb:active', {
+  backgroundColor: `rgb(${theme.color.accent})`,
+});
+
+globalStyle('*::-webkit-scrollbar-track', {
+  background: 'transparent',
 });
