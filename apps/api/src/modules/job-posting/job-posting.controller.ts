752bde19d7914053e98edf40b7e1c1d9fb1da1dc
kimdw524 <kimdw524@naver.com>
2025-08-03 21:57:51 +0900

    feat(be): 북마크한 채용공고 API 구현

--- apps/api/src/modules/job-posting/job-posting.controller.ts
+++ apps/api/src/modules/job-posting/job-posting.controller.ts
@@ -5,11 +5,14 @@ import {
   ParseIntPipe,
   Query,
   Req,
+  UseGuards,
 } from '@nestjs/common';
 
 import { Request } from 'express';
 
+import { AuthSessionGuard } from '../auth-session-guard/auth-session-gaurd';
 import { JobPostingFilterDto } from './dto';
+import { BookmarkedJobPostingFilterDto } from './dto/bookmarked-job-posting-filter.dto';
 import { JobPostingService } from './job-posting.service';
 
 @Controller('job-posting')
@@ -23,6 +26,16 @@ export class JobPostingController {
   ) {
     const userId = req.session.user;
     return this.jobPostingService.getFilteredPostings(userId, filter);
+  }
+
+  @UseGuards(AuthSessionGuard)
+  @Get('/bookmark')
+  getBookmarkedPostings(
+    @Req() req: Request,
+    @Query() filter: BookmarkedJobPostingFilterDto,
+  ) {
+    const userId = req.session.user;
+    return this.jobPostingService.getBookmarkedPostings(userId, filter);
   }
 
   @Get(':id')
--- apps/api/src/modules/job-posting/job-posting.service.ts
+++ apps/api/src/modules/job-posting/job-posting.service.ts
@@ -8,6 +8,7 @@ import { CursorPaginatedResponse } from '@/types/pagination';
 
 import { Bookmark } from '../bookmark/bookmark.entity';
 import { JobPostingFilterDto, JobPostingSummaryDto } from './dto';
+import { BookmarkedJobPostingFilterDto } from './dto/bookmarked-job-posting-filter.dto';
 import { JobPostingDto } from './dto/job-posting.dto';
 import { JobPosting } from './job-posting.entity';
 
@@ -52,6 +53,101 @@ export class JobPostingService {
 
   async decrementBookmark(data: Partial<JobPosting>): Promise<void> {
     await this.jobPostingRepo.decrement(data, 'bookmarks', 1);
+  }
+
+  async getBookmarkedPostings(
+    userId: number | undefined,
+    filter: BookmarkedJobPostingFilterDto,
+  ): Promise<CursorPaginatedResponse<JobPostingSummaryDto>> {
+    const { cursor, limit = 20 } = filter;
+
+    const qb = this.jobPostingRepo
+      .createQueryBuilder('posting')
+      .innerJoin(
+        Bookmark,
+        'bookmark',
+        `bookmark.userId = :userId AND bookmark.targetType = 'job-posting' AND bookmark.targetId = posting.id`,
+        { userId },
+      )
+      .leftJoin('posting.company', 'company')
+      .select([
+        'company.id',
+        'company.name',
+        'company.logo',
+        'posting.id',
+        'posting.title',
+        'posting.openDate',
+        'posting.dueDate',
+        'posting.jobId',
+        'posting.views',
+        'posting.bookmarks',
+        'posting.minExperience',
+        'posting.maxExperience',
+        'posting.employmentType',
+        'bookmark.createdAt',
+      ])
+      .orderBy('bookmark.createdAt', 'DESC')
+      .limit(limit + 1)
+      .addSelect('true as isBookmarked')
+      .distinct(true);
+
+    if (cursor !== undefined) {
+      qb.andWhere('bookmark.createdAt < :cursor', { cursor });
+    }
+
+    const data: {
+      posting_id: number;
+      posting_title: string;
+      posting_open_date: string;
+      posting_due_date: string | null;
+      posting_job_id: number;
+      posting_views: number;
+      posting_bookmarks: number;
+      posting_min_experience: number;
+      posting_max_experience: number;
+      posting_employment_type: number;
+      company_id: number;
+      company_name: string;
+      company_logo: string;
+      bookmark_created_at: string;
+      isBookmarked: 0 | 1;
+    }[] = await qb.getRawMany();
+
+    const transformedData = data.map((i) => ({
+      id: i.posting_id,
+      title: i.posting_title,
+      openDate: i.posting_open_date,
+      dueDate: i.posting_due_date,
+      jobId: i.posting_job_id,
+      views: i.posting_views,
+      bookmarks: i.posting_bookmarks,
+      minExperience: i.posting_min_experience,
+      maxExperience: i.posting_max_experience,
+      employmentType: i.posting_employment_type,
+      company: {
+        id: i.company_id,
+        name: i.company_name,
+        logo: i.company_logo,
+      },
+      bookmark: {
+        createdAt: i.bookmark_created_at,
+      },
+      isBookmarked: i.isBookmarked == 1,
+    }));
+
+    const hasNext = transformedData.length > limit;
+    const slicedData = hasNext
+      ? transformedData.slice(0, limit)
+      : transformedData;
+    const nextCursor = hasNext
+      ? slicedData[slicedData.length - 1].bookmark.createdAt
+      : null;
+
+    return {
+      data: plainToInstance(JobPostingSummaryDto, slicedData),
+      nextCursor,
+      hasNext,
+    };
   }
 
   async getFilteredPostings(
@@ -123,7 +219,7 @@ export class JobPostingService {
     if (cursor !== undefined) {
       qb.andWhere('posting.id < :cursor', { cursor });
     }
-    console.log(userId);
+
     if (userId == undefined) {
       qb.addSelect('false AS `isBookmarked`');
     } else {
@@ -171,7 +267,7 @@ export class JobPostingService {
         name: i.company_name,
         logo: i.company_logo,
       },
-      isBookmarked: i.isBookmarked === 1,
+      isBookmarked: i.isBookmarked == 1,
     }));
 
     const hasNext = transformedData.length > limit;
