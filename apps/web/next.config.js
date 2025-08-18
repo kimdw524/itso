fe396672aff04ad209345742b63704c35fd4524f
kimdw524 <kimdw524@naver.com>
2025-08-19 06:21:28 +0900

    fix(fe): FE 배포 수정 (#28)

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,10 +1,17 @@
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@repo/ui'],
+  output: 'standalone',
+  outputFileTracingRoot: path.join(__dirname, '../../'),
 };
 
 export default withVanillaExtract(nextConfig);
