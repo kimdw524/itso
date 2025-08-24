2bf61d0376a9659655c08eb66e7909d5005fc53b
kimdw524 <kimdw524@naver.com>
2025-08-25 01:58:50 +0900

    feat(useSticky): top 속성값 변경 감지

--- apps/web/src/hooks/useSticky.ts
+++ apps/web/src/hooks/useSticky.ts
@@ -12,22 +12,38 @@ export const useSticky = <T extends HTMLElement>(
       return;
     }
 
+    const createIntersectionObserver = (top: number) => {
+      return new IntersectionObserver(
+        ([entry]) => {
+          setIsStuck(!entry?.isIntersecting);
+        },
+        {
+          rootMargin: `${-top - 1}px 0px 0px 0px`,
+          threshold: 1,
+        },
+      );
+    };
+
     const top = parseInt(getComputedStyle(target).top);
-
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        setIsStuck(!entry?.isIntersecting);
-      },
-      {
-        rootMargin: `${-top - 1}px 0px 0px 0px`,
-        threshold: 1,
-      },
-    );
-
+    let observer = createIntersectionObserver(top);
     observer.observe(target);
+
+    const handleTransitionStart = (e: TransitionEvent) => {
+      if (e.propertyName !== 'top') {
+        return;
+      }
+
+      observer.disconnect();
+      const top = parseInt(getComputedStyle(target).top);
+      observer = createIntersectionObserver(top);
+      observer.observe(target);
+    };
+
+    target.addEventListener('transitionstart', handleTransitionStart);
 
     return () => {
       observer.disconnect();
+      target.removeEventListener('transitionstart', handleTransitionStart);
     };
   }, [targetRef]);
 
