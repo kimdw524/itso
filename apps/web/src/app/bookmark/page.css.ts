f97d6284f844e636a332fbfccff1dbed7ebc2348
kimdw524 <kimdw524@naver.com>
2025-08-30 21:50:52 +0900

    fix(page/bookmark): gird auto-fill로 변경

--- apps/web/src/app/bookmark/page.css.ts
+++ apps/web/src/app/bookmark/page.css.ts
@@ -2,6 +2,6 @@ import { style } from '@vanilla-extract/css';
 
 export const postContainer = style({
   display: 'grid',
-  gridTemplateColumns: 'repeat(auto-fit, minmax(320px, 1fr))',
-  gap: '1.5rem',
+  gridTemplateColumns: 'repeat(auto-fill, minmax(320px, 1fr))',
+  gap: '1.5em',
 });
