e58d8066e236129783483dc3e8e9eb641953a68d
kimdw524 <kimdw524@naver.com>
2025-09-24 05:11:20 +0900

    feat: 최신 공고 추가

--- apps/web/src/app/(home)/page.tsx
+++ apps/web/src/app/(home)/page.tsx
@@ -1,10 +1,8 @@
-import { Suspense } from 'react';
-
 import { Box } from '@repo/ui';
 
 import { Section } from '@/components/Section';
-import { JobPostingRanking } from '@/domains/job-posting/components/JobPostingRanking';
-import { JobPostingRankingLoading } from '@/domains/job-posting/components/JobPostingRanking/loading';
+
+import { JobPostingList } from './_components/JobPostingList';
 
 export default async function HomePage() {
   return (
@@ -17,9 +15,13 @@ export default async function HomePage() {
         title="🔥 인기 공고"
         description="최근 3일 기준으로 조회수가 가장 높은 공고를 정리했어요."
       >
-        <Suspense fallback={<JobPostingRankingLoading />}>
-          <JobPostingRanking />
-        </Suspense>
+        <JobPostingList orderBy="recentViews" />
+      </Section>
+      <Section
+        title="🚀 최신 공고"
+        description="직무와 상관없이 가장 최근에 올라온 공고들이에요."
+      >
+        <JobPostingList orderBy="createdAt" />
       </Section>
     </Box>
   );
