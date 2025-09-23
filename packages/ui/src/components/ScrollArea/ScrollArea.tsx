b3d76ff046441f4b6d1d97c2c256fb8012b6792c
kimdw524 <kimdw524@naver.com>
2025-09-24 04:21:55 +0900

    fix(ScrollArea): right space 계산식 개선

--- packages/ui/src/components/ScrollArea/ScrollArea.tsx
+++ packages/ui/src/components/ScrollArea/ScrollArea.tsx
@@ -33,8 +33,8 @@ export const ScrollArea = ({
     const handleScroll = () => {
       setHasLeftSpace(element.scrollLeft !== 0);
       setHasRightSpace(
-        Math.round(element.scrollLeft + element.clientWidth) <
-          element.scrollWidth,
+        Math.ceil(element.scrollLeft + element.clientWidth) <
+          Math.floor(element.scrollWidth),
       );
     };
 
