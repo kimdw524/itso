d5a35a2f909ea11b674f86b018817d7a2e19bbcb
kimdw524 <kimdw524@naver.com>
2026-02-22 05:23:57 +0900

    feat(fe): 북마크 UI 바로 반영되도록 수정

--- apps/web/src/features/bookmark/services/BookmarkService/hooks/useToggleBookmark.ts
+++ apps/web/src/features/bookmark/services/BookmarkService/hooks/useToggleBookmark.ts
@@ -1,4 +1,10 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
 import type { BookmarkType } from '@/features/bookmark/models';
+import { UserService } from '@/features/user/services';
 
 import { useCreateBookmark } from './useCreateBookmark';
 import { useRemoveBookmark } from './useRemoveBookmark';
@@ -14,15 +20,23 @@ export const useToggleBookmark = ({
   id,
   isBookmarked,
 }: UseToggleBookmarkProps) => {
+  const router = useRouter();
+  const { isSignedIn } = UserService.useUserInfo();
   const { mutate: addBookmark, isPending: isAddPending } = useCreateBookmark({
     type,
     id,
   });
   const { mutate: removeBookmark, isPending: isRemovePending } =
     useRemoveBookmark({ type, id });
+  const [current, setCurrent] = useState<boolean>(isBookmarked);
 
   const toggle = () => {
     const isPending = isAddPending || isRemovePending;
+
+    if (!isSignedIn) {
+      router.push('/sign-in');
+      return;
+    }
 
     if (isPending) {
       return;
@@ -30,11 +44,13 @@ export const useToggleBookmark = ({
 
     if (isBookmarked) {
       removeBookmark();
+      setCurrent(false);
       return;
     }
 
     addBookmark();
+    setCurrent(true);
   };
 
-  return { toggle };
+  return { toggle, current };
 };
