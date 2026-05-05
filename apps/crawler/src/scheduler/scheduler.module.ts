07f114b09c5a180cd45a9abaea804766f269be51
kimdw524 <kimdw524@naver.com>
2026-05-06 05:49:13 +0900

    refactor(crawler): import path 수정

--- apps/crawler/src/scheduler/scheduler.module.ts
+++ apps/crawler/src/scheduler/scheduler.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 
-import { CrawlerModule } from 'src/crawler/crawler.module';
-import { CompanyModule } from 'src/modules/company/company.module';
-import { JobPostingModule } from 'src/modules/job-posting/job-posting.module';
+import { CrawlerModule } from '@/crawler/crawler.module';
+import { CompanyModule } from '@/modules/company/company.module';
+import { JobPostingModule } from '@/modules/job-posting/job-posting.module';
 
 import { JobCrawlerTask } from './tasks/job-crawler.task';
 
