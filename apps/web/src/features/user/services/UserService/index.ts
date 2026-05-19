d7b7b020b4aae3f399a10c67a0b033070aaf94a1
kimdw524 <kimdw524@naver.com>
2026-05-20 03:23:02 +0900

    refactor(fe): User service 리팩토링

--- apps/web/src/features/user/services/UserService/index.ts
+++ apps/web/src/features/user/services/UserService/index.ts
@@ -1,9 +1,7 @@
-import * as hooks from './hooks';
 import * as queries from './queries';
 import { service } from './service';
 
 export const UserService = {
   ...service,
   ...queries,
-  ...hooks,
 };
